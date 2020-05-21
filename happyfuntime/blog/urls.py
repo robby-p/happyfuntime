9f0e0f15fd15c8d1caf800be929f8b443b903a52
kmelich <kmelich@squarespace.com>
2020-05-21 01:39:55 -0400

    basic b blog template bbs

---
+++ happyfuntime/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
+]
